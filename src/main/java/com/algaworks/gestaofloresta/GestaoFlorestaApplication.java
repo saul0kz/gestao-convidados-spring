e11addcb103d869e3cba5df298dce6a1f4352dc0
saulo <saulo@getinfo.net.br>
2020-10-06 20:54:21 -0300

    first commit

---
+++ src/main/java/com/algaworks/gestaofloresta/GestaoFlorestaApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.gestaofloresta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoFlorestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoFlorestaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/gestaofloresta/GestaoFlorestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.gestaofloresta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFlorestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
