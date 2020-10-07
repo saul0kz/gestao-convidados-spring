5ebd2dcdb818f86c3cd282982381b2ddf95964cd
saulo <saulo@getinfo.net.br>
2020-10-07 17:48:20 -0300

    persitindo os dados no banco local

--- src/main/java/com/algaworks/gestaofloresta/controllers/ConvidadosController.java
+++ src/main/java/com/algaworks/gestaofloresta/controllers/ConvidadosController.java
@@ -1,22 +1,33 @@
 package com.algaworks.gestaofloresta.controllers;
 
+import com.algaworks.gestaofloresta.model.Convidado;
 import com.algaworks.gestaofloresta.repository.ConvidadoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/convidados")
 public class ConvidadosController {
 
     @Autowired
     private ConvidadoRepository convidados;
 
-    @GetMapping("/convidados")
-    public ModelAndView listar() {
-        ModelAndView modelAndView = new ModelAndView("ListaConvidados");
+    @PostMapping
+    public String salvar(Convidado convidado) {
+        this.convidados.save(convidado);
+        return "redirect:/convidados";
+    }
 
+    @GetMapping
+    public ModelAndView listar() {
+
+        ModelAndView modelAndView = new ModelAndView("ListaConvidados");
+        modelAndView.addObject(new Convidado());
         modelAndView.addObject("convidados", convidados.findAll());
 
         return modelAndView;
